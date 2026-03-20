f63497f97e2755dd6eb7b0171904d2d1a096b583
Saron <Saron.okbamichael@edu.sait.ca>
2026-03-20 16:10:44 -0400

    Fix layout and update Week 9 auth context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import "./globals.css";
 import { AuthContextProvider } from "./contexts/autho-context";
 
+export const metadata = {
+  title: "CPRG 306 Assignments",
+  description: "Student project for Week 9",
+};
+
 export default function RootLayout({
   children,
 }: {
